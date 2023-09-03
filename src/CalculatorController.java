89e7e0dce8c82b79bce7497b90418a779b3c39d6
LogicalSpaghetti <logicalspaghetti@gmail.com>
2023-09-02 23:24:57 -0400

    First commit

---
+++ src/CalculatorController.java
@@ -0,0 +1,37 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorController {
+
+    private CalculatorView theView;
+    private CalculatorModel theModel;
+
+    public CalculatorController(CalculatorView theView, CalculatorModel theModel) {
+        this.theView = theView;
+        this.theModel = theModel;
+
+        this.theView.addCalculateListener(new CalculateListener());
+    }
+
+    class CalculateListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            int firstNumber, secondNumber = 0;
+
+            try {
+                firstNumber = theView.getFirstNumber();
+                secondNumber = theView.getSecondNumber();
+
+                theModel.addTwoNumbers(firstNumber, secondNumber);
+
+                theView.setCalcSolution(theModel.getCalculationValue());
+            } catch (NumberFormatException ex) {
+
+                theView.disPlayErrorMessage("You Need to Enter 2 Integers");
+
+            }
+
+        }
+    }
+}
